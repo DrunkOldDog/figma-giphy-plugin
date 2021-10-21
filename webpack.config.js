88ae612cfb5ef061bdd51739b455ea7311d3f3c1
Juani Reyes <juanisoul@gmail.com>
2021-10-20 23:40:36 -0400

    Fix webpack issue of document as null

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,7 @@ module.exports = (env, argv) => ({
       filename: "ui.html",
       inlineSource: ".(js)$",
       chunks: ["ui"],
+      inject: "body",
     }),
     new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
   ],
